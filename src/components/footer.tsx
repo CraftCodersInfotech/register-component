55971a7b64ecbf9483a84065ef088f3fa42d7b23
Hitesh Kachariya <hiteshk086@gmail.com>
2023-01-23 11:46:29 +0530

    initial commit

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,11 +1,12 @@
 import { StyleSheet, Text, View } from "react-native";
 import * as React from "react";
-import Button from "react-native-button";
+// import Button from "react-native-button";
 import { styles } from "../styles";
 import { String } from "../constant/String";
 import * as yup from "yup";
 import { useForm } from "react-hook-form";
 import { yupResolver } from "@hookform/resolvers/yup";
+import {Button} from "theme-component"
 
 export type props = {
   onPress: () => void;
@@ -16,14 +17,19 @@ const footer = (props: props) => {
 
   return (
     <View style={{ marginTop: 20 }}>
-      <Button
+      {/* <Button
         title={"Continue"}
         containerStyle={styles.containerStyleFooter}
         textStyle={styles.textStyleFooter}
         backgroundColor={"#5625CC"}
         activeBackgroundColor={"#7f52e9"}
         onPress={onPress}
-      />
+      /> */}
+       <Button
+       style={styles.ripple}
+        onPress={() => {
+         console.log('red');
+     }}></Button>
       <View style={styles.footer}>
         <Text style={styles.Font}>
           {String.description}
